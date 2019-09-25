636b5db6490e67f3daa0009d0a2fed07146a5c5d
Gabriel Piles González <gabo@Gabriels-MacBook-Pro.local>
2019-09-25 17:17:02 +0200

    Add money unit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Input from './input/ExpenseInput';
 import 'semantic-ui-css/semantic.min.css'
+import MoneyUnit from './moneyUnit/MoneyUnit';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Input></Input>
+        {/* <Input></Input> */}
+        <MoneyUnit></MoneyUnit>
       </header>
     </div>
   );
---
+++ src/moneyUnit/MoneyUnit.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './moneyUnit.css';
+
+
+const MoneyUnit: React.FC = () => {
+  return (
+    <div className="money-unit"></div>
+  )
+}
+
+export default MoneyUnit;
