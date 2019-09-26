34a7fb95b14ff6619cdd87f6c57efc424901cc0c
Gabriel Piles González <gabriel.piles.glez@gmail.com>
2019-09-26 13:49:16 +0200

    Add several animations to money unit

--- src/moneyUnit/MoneyUnit.tsx
+++ src/moneyUnit/MoneyUnit.tsx
@@ -1,14 +1,23 @@
 import React, { createRef, useEffect } from 'react';
 import './moneyUnit.css';
-import { TweenLite } from 'gsap';
+import { TimelineLite, Power3 } from 'gsap';
 
 
 const MoneyUnit: React.FC = () => {
   let moneyUnitRef = createRef<HTMLDivElement>();
 
+  function someFunction(){
+      console.log('it worked');
+  }
+
   useEffect(() => {
-    // Update the document title using the browser API
-    TweenLite.to(moneyUnitRef.current, 1, { opacity: 1 });
+    var timelineLite = new TimelineLite({onComplete:someFunction});
+    if(moneyUnitRef.current){
+      timelineLite.to(moneyUnitRef.current, .2, { opacity: 1, ease: Power3.easeInOut})
+      .to(moneyUnitRef.current, 1.5, { x:100, ease: Power3.easeInOut})
+      .to(moneyUnitRef.current, 1.5, { x:-100, ease: Power3.easeInOut})
+      .to(moneyUnitRef.current, 1.5, { x:100, ease: Power3.easeInOut});
+    }
   });
 
   return (
