a40aad59f8c4eb2a7f61db65ccf9096f7fe765ee
Gabriel Piles González <gabo@Gabriels-MacBook-Pro.local>
2019-09-26 13:23:40 +0200

    Add opacity effect to money unit

--- src/moneyUnit/MoneyUnit.tsx
+++ src/moneyUnit/MoneyUnit.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { createRef, useEffect } from 'react';
 import './moneyUnit.css';
+import { TweenLite } from 'gsap';
 
 
 const MoneyUnit: React.FC = () => {
+  let moneyUnitRef = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    TweenLite.to(moneyUnitRef.current, 1, { opacity: 1 });
+  });
+
   return (
-    <div className="money-unit"></div>
+    <div ref={moneyUnitRef} className="money-unit"></div>
   )
 }
 
